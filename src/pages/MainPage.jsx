66214dd1a0a7bdd53eb6cdd1f6b028819549fc01
Lerry <lerry@Lerryui-MacBookPro.local>
2022-01-03 22:28:02 +0900

    [feat] pages/ LoginPage, SignupPage, MainPage 컴포넌트 추가

---
+++ src/pages/MainPage.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function MainPage() {
+  return (
+    <>
+      <p>메인 페이지</p>
+    </>
+  );
+}
---
+++ src/pages/SignupPage.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function SignupPage() {
+  return (
+    <>
+      <p>회원가입</p>
+    </>
+  );
+}
