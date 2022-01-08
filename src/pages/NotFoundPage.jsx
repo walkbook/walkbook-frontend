d3aa3e34983d095946489c3b4d76fad222a73645
Lerry <lerry@Lerryui-MacBookPro.local>
2022-01-09 04:29:04 +0900

    [chore] 모듈 설치

--- src/pages/NotFoundPage.jsx
+++ src/pages/NotFoundPage.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 export default function NotFoundPage() {
   return (
     <>
-      <p>WalkBook : 페이지를 찾을 수 없습니다.</p>
+      <p>WalkBook : 잘못된 접근입니다. 돌아가세요!</p>
     </>
   );
 }
