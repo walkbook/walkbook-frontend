52454cf341990a84eec3dd1466f07f73f62ed50f
Lerry <lerry597@gmail.com>
2022-01-29 20:25:49 +0900

    [feature] postlist 띄우기

--- src/_reducers/user_reducer.js
+++ src/_reducers/user_reducer.js
@@ -1,5 +1,5 @@
 const initialState = {
-  name: 'yeji',
+
 }
 
 export default function reducer(state = initialState, action) {
--- src/components/common/PostcardList.jsx
+++ src/components/common/PostcardList.jsx
@@ -1,12 +1,34 @@
 import Postcard from './Postcard';
+import { useDispatch, useSelector } from 'react-redux';
+import { get } from '../../utils/utils';
+import { requestPostPage } from '../../_actions/user_action';
 
 export default function PostcardList() {
+  const dispatch = useDispatch();
+  // page, size, sort 관련된 것 불러와주기 아마도 container이나 page에서 내려줘야..?! 
+  const page = 0
+  const size = 8
+  const sort = 'createdDate';
+  dispatch(requestPostPage(page, size, sort));
+  const pageContent = useSelector(get('pageContent'));
+  
+  if (!pageContent) {
+    return (
+      <p>Loading...</p>
+    );
+  }
+
   return (
-    <Postcard 
-      postId={postId}
-      title={title}
-      authorName={authorName}
-      description={description}
-    />
+    <>
+      {pageContent.map((post) => (
+        <Postcard
+          key={post.postId}
+          postId={post.postId}
+          title={post.title}
+          authorName={post.authorName}
+          description={post.description}
+        />
+      ))}
+    </>
   )
 }
